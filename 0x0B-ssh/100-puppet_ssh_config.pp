488a47541052ca08f82e226d23367e345304c681
Vikitohyah <victoriaedorh@gmail.com>
2023-06-28 05:18:13 -0700

    using puppet to config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# To be able to use puppet to make chabges to my ssh configuration file
+# To be able to use puppet to make changes to my ssh configuration file
 
-file { 'etc/ssh/ssh_config':
+file { 'ect/ssh/ssh_config':
 	ensure => present,
 
 content =>"
