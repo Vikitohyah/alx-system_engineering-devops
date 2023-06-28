66608b9514e7b85e581efd44a6e8f972b27c51a2
Vikitohyah <victoriaedorh@gmail.com>
2023-06-28 05:49:46 -0700

    using puppet to config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 # To be able to use puppet to make changes to my ssh configuration file
+# sets up a client SSH configuration file so that we can connect to a server without using a password
+#include stdlib
+#file_line { 'Declare identity file':
+#  path    => '/etc/ssh/ssh_config',
+#  line    => '    IdentityFile ~/.ssh/school',
+#  replace => true,
+#}
 
-file { 'etc/ssh/ssh_config':
-	ensure => present,
+#file_line { 'Turn off passwd auth':
+#  path    => '/etc/ssh/ssh_config',
+#  line    => '    PasswordAuthentication no',
+#  replace => true,
+#}
 
-content =>"
+# client configuration file
 
-	#SSH client configuration
-	host°
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	",
-
+$file_content = file('/etc/ssh/ssh_config')
+$config = "${file_content}\
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+"
+file {  'school':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  content => $config
 }
